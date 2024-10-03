376a1da56d6efae2e1766bade8f9ae7f169b60f2
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-03 20:45:34 +0300

    yalla

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,15 +5,15 @@ export default class Currency {
   }
 
   get code() {
-    return this.code;
-  }
-
-  set code(newCode) {
-    this._code = newCode;
+    return this._code;
   }
 
   get name() {
     return this._name;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
   }
 
   set name(newName) {
