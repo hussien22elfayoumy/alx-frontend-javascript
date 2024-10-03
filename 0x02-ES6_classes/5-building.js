943ab7358a46b2231b1b702d782a6178e77bd5ea
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-03 21:29:08 +0300

    yalla

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) this.evacuationWarningMessage();
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage'
+    );
+  }
+}
