afb06d727509160822f6a36e92d6518190a167c2
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-03 20:34:16 +0300

    yaraaaab

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,18 +3,15 @@ export default class HolbertonCourse {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-
     if (
       !Array.isArray(students) ||
       !students.every((student) => typeof student === 'string')
     ) {
       throw new TypeError('Students must be an array of strings');
     }
-
     this._name = name;
     this._length = length;
     this._students = students;
@@ -28,7 +25,6 @@ export default class HolbertonCourse {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
-
     this._name = newName;
   }
 
@@ -40,7 +36,6 @@ export default class HolbertonCourse {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
-
     this._length = newLength;
   }
 
@@ -55,7 +50,6 @@ export default class HolbertonCourse {
     ) {
       throw new TypeError('Students must be an array of strings');
     }
-
     this._students = newStudents;
   }
 }
