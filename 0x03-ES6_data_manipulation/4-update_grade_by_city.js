11541be531642d3885b4eb222a93584cffd12252
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-07 21:14:37 +0300

    ignore

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
-    .map((student, i) => {
+    .map((student) => {
       // prettier-ignore
       const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
       return { ...student, grade: gradeObject ? gradeObject.grade : 'N/A' };
