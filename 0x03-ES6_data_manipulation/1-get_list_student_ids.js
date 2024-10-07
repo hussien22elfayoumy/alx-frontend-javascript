0c04a609c11cf5a0794d71bc092acee639f9c386
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-07 19:51:12 +0300

    array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,5 @@
 export default function getListStudentIds(students) {
-  if (typeof students === 'object') {
-    const ids = students.map((student) => student.id);
-    return ids;
+  if (Array.isArray(students)) {
+    return students.map((student) => student.id);
   } else return [];
 }
