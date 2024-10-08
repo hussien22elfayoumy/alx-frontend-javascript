3da046d45f7358584a1490e7724b89ab96c2d98a
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-08 20:03:10 +0300

    mashy

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set)) return '';
+  const arr = [];
 
   if (startString.length === 0) return '';
-
-  const arr = [];
 
   set.forEach((ele) => {
     if (ele.startsWith(startString)) arr.push(ele.slice(startString.length));
