102450ac7ae4c04e8a1428227d57eac35f082e3c
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-09 20:45:49 +0300

    teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,21 @@
-import { Teacher } from '../main';
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
 
 interface Directors extends Teacher {
   numberOfReports: number;
@@ -11,5 +28,14 @@ const director1: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
-console.log('hello');
 console.log(director1);
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = function (firstName, lastName) {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe'));
